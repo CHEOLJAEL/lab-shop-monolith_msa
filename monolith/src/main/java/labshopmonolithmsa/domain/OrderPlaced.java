ae821c1376dfca6f6e7be48dce7325fdb7013d72
CHEOLJAEL <120994895+CHEOLJAEL@users.noreply.github.com>
2022-12-22 10:45:13 +0900

    commit

---
+++ monolith/src/main/java/labshopmonolithmsa/domain/OrderPlaced.java
@@ -0,0 +1,24 @@
+package labshopmonolithmsa.domain;
+
+import labshopmonolithmsa.domain.*;
+import labshopmonolithmsa.infra.AbstractEvent;
+import java.util.*;
+import lombok.*;
+
+
+@Data
+@ToString
+public class OrderPlaced extends AbstractEvent {
+
+    private Long id;
+    private String productId;
+    private Integer qty;
+    private String customerId;
+
+    public OrderPlaced(Order aggregate){
+        super(aggregate);
+    }
+    public OrderPlaced(){
+        super();
+    }
+}
